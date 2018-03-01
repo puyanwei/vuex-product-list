7ab4c3f28a861cdc5927505c5ac01b415910b154
Puyan Wei <puyanwei@gmail.com>
2018-03-01 17:44:46 +0000

    imported mapActions and mapGetters into productListOne.vue, and used spread operator to call them with

--- src/components/productListOne.vue
+++ src/components/productListOne.vue
@@ -12,19 +12,18 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
+import { mapGetters } from "vuex";
+
 export default {
   computed: {
     products() {
       return this.$store.state.products;
     },
-    saleProducts() {
-      return this.$store.getters.saleProducts;
-    }
+    ...mapGetters(["saleProducts"])
   },
   methods: {
-    reducePrice: function(amount) {
-      this.$store.dispatch("reducePrice", amount);
-    }
+    ...mapActions(["reducePrice"])
   }
 };
 </script>
