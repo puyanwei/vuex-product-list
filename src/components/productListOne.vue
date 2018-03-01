7efc55cfb139c3bfc46b27e11b400a95d217b072
Puyan Wei <puyanwei@gmail.com>
2018-03-01 17:08:18 +0000

    added button to reduce price of products added method reducePrice that loops through each product in state.js and reduce the price by one

--- src/components/productListOne.vue
+++ src/components/productListOne.vue
@@ -7,6 +7,7 @@
       <span class="price">£{{ product.price }}</span>
     </li>
   </ul>
+  <button @click="reducePrice">Reduce Price</button>
 </div>
 </template>
 
@@ -18,6 +19,13 @@ export default {
     },
     saleProducts() {
       return this.$store.getters.saleProducts;
+    }
+  },
+  methods: {
+    reducePrice: function() {
+      this.$store.state.products.forEach(product => {
+        product.price -= 1;
+      });
     }
   }
 };
