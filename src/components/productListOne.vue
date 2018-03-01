927423d0ad3671734bb6305c18e799855e5d8102
Puyan Wei <puyanwei@gmail.com>
2018-03-01 14:58:49 +0000

    output product list one and two onto page using props

--- src/components/productListOne.vue
+++ src/components/productListOne.vue
@@ -2,13 +2,17 @@
 <div id="product-list-one">
   <h2>Product List One</h2>
   <ul>
-    <li v-for""></li>
+    <li v-for="product in products">
+      <span class="name">{{ product.name }}</span>
+      <span class="price">£{{ product.price }}</span>
+    </li>
   </ul>
 </div>
 </template>
 
 <script>
 export default {
+  props: ["products"],
   name: "app",
   data() {
     return {};
--- src/components/productListTwo.vue
+++ src/components/productListTwo.vue
@@ -2,13 +2,17 @@
 <div id="product-list-two">
   <h2>Product List Two</h2>
   <ul>
-    <li v-for""></li>
+    <li v-for="product in products">
+      <span class="name">{{ product.name }}</span>
+      <span class="price">£{{ product.price }}</span>
+    </li>
   </ul>
 </div>
 </template>
 
 <script>
 export default {
+  props: ["products"],
   name: "app",
   data() {
     return {};
