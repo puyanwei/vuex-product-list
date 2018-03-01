cb826dba7bfbd813808aaddcb00d09871676634f
Puyan Wei <puyanwei@gmail.com>
2018-03-01 15:11:50 +0000

    Setting up a Central Store - moved data from Vue.js to store.js and exported it

--- src/App.vue
+++ src/App.vue
@@ -16,14 +16,7 @@ export default {
   },
   name: "app",
   data() {
-    return {
-      products: [
-        { name: "Sylvanas", price: 1600 },
-        { name: "Reno Jackson", price: 1000 },
-        { name: "Edwin VanCleef", price: 850 },
-        { name: "Elise Starseeker", price: 1200 }
-      ]
-    };
+    return {};
   }
 };
 </script>
--- src/store/store.js
+++ src/store/store.js
@@ -2,3 +2,14 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  state: {
+    products: [
+      { name: "Sylvanas", price: 1600 },
+      { name: "Reno Jackson", price: 1000 },
+      { name: "Edwin VanCleef", price: 850 },
+      { name: "Elise Starseeker", price: 1200 }
+    ]
+  }
+});
